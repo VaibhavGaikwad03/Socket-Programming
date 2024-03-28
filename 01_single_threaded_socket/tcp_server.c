e8e356d97656bff930450a709ee8ea35f0d8c162
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-03-28 20:48:38 +0530

    Add files via upload

--- 01_single_threaded_socket/tcp_server.c
+++ 01_single_threaded_socket/tcp_server.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
 #include <netdb.h>
 #include <memory.h>
 #include <errno.h>
@@ -20,7 +22,11 @@ void setup_tcp_server_communication()
     /* Master socket file descriptor, used to accept new client connection only, no data exchange */
     int master_sock_tcp_fd = 0, sent_recv_bytes = 0, addr_len = 0, opt = 1;
 
-    /* Client specific  */
+    /* Client specific communication socket file descriptor, used for only data exchange/communication between client and server */
+    int comm_socket_fd = 0;
+    /* Set of file descriptor on which select() polls. select() unblocks whenever data arrives on any fd present in this set */
+    fd_set readfds;
+    /* Variables to hold server information */
 }
 
 int main(void)
