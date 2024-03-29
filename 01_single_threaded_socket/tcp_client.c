15ff02be5e09b35bc35639e7d91cb39a1a8100ca
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-03-29 20:38:35 +0530

    Add files via upload

--- 01_single_threaded_socket/tcp_client.c
+++ 01_single_threaded_socket/tcp_client.c
@@ -35,7 +35,7 @@ void setup_tcp_communication()
     our client is interested in sending data to server process which is listening on PORT = DEST_PORT */
     dest.sin_port = DEST_PORT;
     struct hostent *host = (struct hostent *)gethostbyname(SERVER_IP_ADDRESS);
-    dest.sin_addr = ((struct in_addr *)host->h_addr);
+    dest.sin_addr = *((struct in_addr *)host->h_addr);
 
     /* Step 3: create a TCP socket */
     /* Create a socket finally. socket() is a system call, which asks for three parameters */
@@ -45,27 +45,36 @@ void setup_tcp_communication()
 
     /* Step 4: get the data to be sent to server */
     /* Out client is now ready to send data to server. sendto() sends data to server */
-PROMPT_USER:
 
-    /* Prompt the user to enter data */
-    printf("Enter A: ");
-    scanf("%u", &client_data.a);
-    printf("Enter B: ");
-    scanf("%u", &client_data.b);
+    while (1)
+    {
+        /* Prompt the user to enter data */
+        printf("Enter A: ");
+        scanf("%u", &client_data.a);
+        printf("Enter B: ");
+        scanf("%u", &client_data.b);
 
-    /* Step 5: send the data to server */
-    sent_recv_bytes = sendto(sockfd, &client_data, sizeof(test_struct_t), 0, (struct sockaddr *)&dest, sizeof(struct sockaddr));
+        /* Step 5: send the data to server */
+        sent_recv_bytes = sendto(sockfd, &client_data, sizeof(test_struct_t), 0, (struct sockaddr *)&dest, sizeof(struct sockaddr));
 
-    printf("Number of bytes sent = %d\n", sent_recv_bytes);
+        printf("Number of bytes sent = %d\n", sent_recv_bytes);
 
-    /* Step 6: client also want to reply from server after sending data */
-    /* recvfrom is a blocking system call, meaning the client program will not run past this point
-    untill the data arrives on the socket from server */
-    sent_recv_bytes = recvfrom(sockfd, (char *)&result, sizeof(result_struct_t), 0, (struct sockaddr *)&dest, &addr_len);
+        /* Step 6: client also want to reply from server after sending data */
+        /* recvfrom is a blocking system call, meaning the client program will not run past this point
+        untill the data arrives on the socket from server */
+        sent_recv_bytes = recvfrom(sockfd, (char *)&result, sizeof(result_struct_t), 0, (struct sockaddr *)&dest, &addr_len);
 
-    printf("Number of bytes received = %d\n", sent_recv_bytes);
-    printf("Result received = %u\n", result.c);
+        printf("Number of bytes received = %d\n", sent_recv_bytes);
+        printf("Result received = %u\n", result.c);
 
-    /* Step 7: client would want to send the data again to the server, go into infinite loop */
-    goto PROMPT_USER;
+        /* Step 7: client would want to send the data again to the server, go into infinite loop */
+    }
+}
+
+int main(void)
+{
+    setup_tcp_communication();
+    printf("Application quits...\n");
+
+    return 0;
 }
